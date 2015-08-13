b46f85977ccc49bc7d3cc4bffee07df29bc4cd7c
Kristie <kristietamato@gmail.com>
2015-08-12 19:49:14 -0700

    creating a single player activity  and it's intent

---
+++ app/src/main/java/org/tamato/hangdroid/GameActivity.java
@@ -0,0 +1,16 @@
+package org.tamato.hangdroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+}
