e795cf176eaef162e51a39d2fdbde4cc092b30a4
Kristie <kristietamato@gmail.com>
2015-08-14 12:29:55 -0700

    changing image with failed letter and showing failed letter(s)

--- app/src/main/java/org/tamato/hangdroid/GameActivity.java
+++ app/src/main/java/org/tamato/hangdroid/GameActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,6 +13,9 @@ import android.widget.Toast;
 public class GameActivity extends AppCompatActivity
 {
     String mWord = "WORD";
+
+    int nFailCounter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -41,15 +45,62 @@ public class GameActivity extends AppCompatActivity
     {
         char charInput = letterInput.charAt(0);
 
+        boolean letterGuessed = false;
+
         for (int counter = 0; counter < mWord.length(); counter++)
         {
             char charFromTheWord = (mWord.charAt(counter));
             if (charFromTheWord == charInput)
             {
                 Log.d("MYLOG", "There was one match");
+
+                letterGuessed = true;
+
                 showLetterAtIndex(counter, charInput);
             }
         }
+
+        if (letterGuessed == false)
+        {
+            letterFailed(Character.toString(charInput));
+        }
+    }
+
+    public void letterFailed(String letterFailed)
+    {
+        showFailedLetter(letterFailed);
+
+        nFailCounter++;
+
+        // change the image
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        switch (nFailCounter)
+        {
+            case 1: imageView.setImageResource(R.drawable.hangdroid_1);
+                    break;
+            case 2: imageView.setImageResource(R.drawable.hangdroid_2);
+                    break;
+            case 3: imageView.setImageResource(R.drawable.hangdroid_3);
+                    break;
+            case 4: imageView.setImageResource(R.drawable.hangdroid_4);
+                    break;
+            case 5: imageView.setImageResource(R.drawable.hangdroid_5);
+                    break;
+            case 6: //TODO Game over
+                    break;
+        }
+    }
+
+    // show failed letter(s)
+    public void showFailedLetter(String failedLetter)
+    {
+        TextView textViewFailed = (TextView) findViewById(R.id.failedLetters);
+
+        // get the previous failed letter
+        String previousFailed = textViewFailed.getText().toString();
+
+        textViewFailed.setText(previousFailed + failedLetter);
     }
 
     // show the correctly guessed letter
